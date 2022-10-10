a2653f4823c555d1d9828fb46a14bd68734bfd44
Alex <alexis.delahaye7@gmail.com>
2022-10-10 13:12:17 +0200

    add changeBgColor + changeBrushColor + clearGrid

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
+/* Default values */
+
 const DEFAULT_SIZE = 16;
-const DEFAULT_COLOR = '#444';
+const DEFAULT_COLOR_BRUSH = '#fff';
+const DEFAULT_COLOR_BACKGROUND = "#444";
+
+/* Adjustable values */
 
 let gridSize = DEFAULT_SIZE;
-let brushColor = DEFAULT_COLOR;
+let brushColor = DEFAULT_COLOR_BRUSH;
+let backgroundColor = DEFAULT_COLOR_BACKGROUND;
 
+/* Set default color-picker "color" value in HTML */
+
+const colorPickerBrush = document.getElementById('color-picker-brush');
+colorPickerBrush.setAttribute('color', DEFAULT_COLOR_BRUSH);
+const colorPickerBackground = document.getElementById('color-picker-background');
+colorPickerBackground.setAttribute('color', DEFAULT_COLOR_BACKGROUND);
+
+/* Initialize/Change background color */
 
 const grid = document.getElementById('grid');
+changeBgColor(backgroundColor);
 
+function changeBgColor(backgroundColor){
+    grid.style.backgroundColor = backgroundColor;
+    console.log('ok');
+}
 
-setupGrid(gridSize);
-
+/* Initialize grid */
 
 function setupGrid(size){
     for(let i = 0; i < size*size; i++){
         grid.innerHTML += "<div class='square'></div>";
     }
 }
+
+setupGrid(gridSize);
+
+/* Change squares color on hover */
 
 const squares = document.querySelectorAll('.square');   //grid is set up then, elements are selected (obviously)
 
@@ -25,8 +47,22 @@ squares.forEach((square) => {
     });
 });
 
-/*
-function setColor(){
-    this.style.backgroundColor = "red";
-}
-*/
+/* Change brush color */
+
+colorPickerBrush.addEventListener('change', (e) => {
+    brushColor = e.target.value;
+});
+
+/* Change background color */
+
+colorPickerBackground.addEventListener('change', (e) => {
+    backgroundColor = e.target.value;
+    changeBgColor(backgroundColor);
+})
+
+/* Clear grid */
+
+const clearGrid = document.getElementById('clear-grid');
+clearGrid.addEventListener('click', ()=>{
+    squares.forEach((element) => element.style.backgroundColor = "")
+})
